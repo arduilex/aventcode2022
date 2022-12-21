db4d93e5ad25a71785aa3274a94f2f9ee4ecc236
arduilex <alexandrebader3@gmail.com>
2022-12-22 00:35:55 +0100

    fix ascci to unicode

--- day3/part2.py
+++ day3/part2.py
@@ -21,12 +21,12 @@ def sum_priorities_group()->int:
     return total
 
 priorities = {}
-# chr translate ascii code in a letter of the ascii table
+# chr translate unicode code in a letter of the unicode table
 # a-z -> 97-122 
-for i, ascii_letter in enumerate(range(97, 123), start=1):
-    priorities[chr(ascii_letter)] = i
+for i, unicode_letter in enumerate(range(97, 123), start=1):
+    priorities[chr(unicode_letter)] = i
 # A-Z -> 65-90
-for i, ascii_letter in enumerate(range(65, 91), start=27):
-    priorities[chr(ascii_letter)] = i
+for i, unicode_letter in enumerate(range(65, 91), start=27):
+    priorities[chr(unicode_letter)] = i
     
 print(sum_priorities_group())    
