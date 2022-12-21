1ab8d8edb9704ad79d02a498d1a1e32143a3cd5c
arduilex <alexandrebader3@gmail.com>
2022-12-21 21:08:35 +0100

    add part2 in part1 code

---
+++ day1/part1+2.py
@@ -0,0 +1,14 @@
+f=open("input.txt", 'r', encoding="utf-8")
+food = [[]]
+for line in f:
+    if line != '\n':
+        food[-1].append(int(line[:-1]))
+    else:
+        food.append([])
+somme = [0]*len(food)
+for i, miam in enumerate(food):
+    somme[i] = sum(miam)
+somme = sorted(somme, reverse=True)
+
+print("part1:",somme[0])
+print("part2:",sum(somme[0:3]))
