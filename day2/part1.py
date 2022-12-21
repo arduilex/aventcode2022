39e1476ba7b5875ad420dd2f3e36f6088ae12a0a
arduilex <alexandrebader3@gmail.com>
2022-12-21 22:43:07 +0100

    finish day2

---
+++ day2/part1.py
@@ -0,0 +1,16 @@
+def match(p1:int, p2:int)->int:
+    point = 0
+    if (p1,p2) in [(3, 1), (1,2), (2,3)]:
+            point = 6
+    elif p1==p2:
+        point = 3
+    return point+p2
+
+def final_score()->int:
+    total = 0
+    with open("input.txt", 'r', encoding="utf-8") as f:
+        for line in f:
+            total += match(ord(line[0])-64, ord(line[-2])-87)
+    return total
+
+print(final_score())
---
+++ day2/part2.py
@@ -0,0 +1,35 @@
+# X = loose 
+# Y = draw
+# Z = win
+translate = {"X":"loose",
+            "Y":"draw",
+            "Z":"win"}
+win_against = {3:1,1:2,2:3}
+loose_against = {1:3,2:1,3:2}
+
+def match(p1:int, p2:int)->int:
+    point = 0
+    if (p1,p2) in [(3, 1), (1,2), (2,3)]:
+        point = 6
+    elif p1==p2:
+        point = 3
+    return point+p2
+
+def select(p1:int, action:str)->int:
+    if translate[action] == "win":
+        return win_against[p1]
+    elif translate[action] == "loose":
+        return loose_against[p1]
+    else:
+        return p1
+        
+def final_score()->int:
+    total = 0
+    with open("input.txt", 'r', encoding="utf-8") as f:
+        for line in f:
+            p1_int = ord(line[0])-64
+            p2_str = line[-2]
+            total += match(p1_int, select(p1_int, p2_str))
+    return total
+
+print(final_score())
